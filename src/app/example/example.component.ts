3775e0bd4981b8fdaa9831bf19edddd624c788ce
David Reyes Pucheta <davidpucheta@gmail.com>
2023-01-25 12:27:33 -0600

    Pass inputs to components

---
+++ src/app/example/example.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  templateUrl: './example.component.html',
+  styleUrls: ['./example.component.css']
+})
+export class ExampleComponent {
+  @Input() name: string = "";
+}
