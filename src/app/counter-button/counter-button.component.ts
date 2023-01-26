ab90d4df86263640b24964231b2b7d4be18f724d
David Reyes Pucheta <davidpucheta@gmail.com>
2023-01-26 11:16:21 -0600

    Use lifecycle events in components

--- src/app/counter-button/counter-button.component.ts
+++ src/app/counter-button/counter-button.component.ts
@@ -1,13 +1,21 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-counter-button',
   templateUrl: './counter-button.component.html',
   styleUrls: ['./counter-button.component.css']
 })
-export class CounterButtonComponent {
+export class CounterButtonComponent implements OnInit, OnDestroy {
   @Input() count:number = 0;
   @Output() buttonClicked = new EventEmitter<void>();
+
+  ngOnDestroy(): void {
+    alert('In ngOnDestroy');
+  }
+
+  ngOnInit(): void {
+    alert('In ngOnInit');
+  }
 
   onClick(): void{
     this.buttonClicked.emit();
