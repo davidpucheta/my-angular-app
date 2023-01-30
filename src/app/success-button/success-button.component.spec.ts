3be981b5f3bab357581b64ef80f362f6d6b3fead
David Reyes Pucheta <davidpucheta@gmail.com>
2023-01-30 17:13:06 -0600

    difference between Inheritance and Composition

---
+++ src/app/success-button/success-button.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SuccessButtonComponent } from './success-button.component';
+
+describe('SuccessButtonComponent', () => {
+  let component: SuccessButtonComponent;
+  let fixture: ComponentFixture<SuccessButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SuccessButtonComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SuccessButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
